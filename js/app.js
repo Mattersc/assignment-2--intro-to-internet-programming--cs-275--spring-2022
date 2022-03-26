0c5502414b7fded1224dfb9ce0f55126d8be489d
Mattersc <98325497+Mattersc@users.noreply.github.com>
2022-03-25 23:36:24 -0400

    updated javascript

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ if (RiceRecipe != null) {
     document.getElementById("Recipe").innerHTML ="You have selected no amount to cook";
 }
 if(RiceRecipe=="White rice")
-    document.getElementById("Recipe").innerHTML="Combine "+RiceAmount+" cups of rice with "+RiceAmount*2+" cups of water and "+RiceAmount+" Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about "+(18+((RiceAmount-1)*10))+" minutes.";
+    document.getElementById("Recipe").innerHTML=`Combine ${RiceAmount} cups of rice with ${RiceAmount*2} cups of water and ${RiceAmount} Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about ${(18+((RiceAmount-1)*10))} minutes.`;
 else if(RiceRecipe=="Sprouted California rice")
-    document.getElementById("Recipe").innerHTML="Combine "+RiceAmount+ " cups "+"of rice with "+RiceAmount*1.75+" cups of water or broth and "+RiceAmount+" Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for "+(25+((RiceAmount-1)*10))+" minutes. Remove from heat and let stand for "+5*RiceAmount+" minutes. Fluff with a fork and serve.";
+    document.getElementById("Recipe").innerHTML=`Combine ${RiceAmount} cups of rice with ${RiceAmount*1.75} cups of water or broth and ${RiceAmount} Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for "+(25+((RiceAmount-1)*10))+" minutes. Remove from heat and let stand for "+5*RiceAmount+" minutes. Fluff with a fork and serve.`;
 else
     document.getElementById("Recipe").innerHTML="Please fill in every option ";
 }
